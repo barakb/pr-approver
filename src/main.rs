0e5254ac19e13854fb8357c12def21228a585950
Barak Bar Orion <barak.bar@gmail.com>
2024-05-24 12:30:09 +0300

    Request and approve pr by renovate provided that all checks passed

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use octocrate::{APIConfig, GitHubAPI, PersonalAccessToken, PullRequestReview, PullRequestSimple, PullsSubmitReviewRequest, PullsSubmitReviewRequestEvent, Request, SharedAPIConfig};
+use octocrate::{APIConfig, CheckRunConclusion, ChecksListForRefResponse, GitHubAPI, PersonalAccessToken, PullRequestReview, PullRequestSimple, PullsSubmitReviewRequest, PullsSubmitReviewRequestEvent, Request, SharedAPIConfig};
 use serde::{Deserialize, Serialize};
 
 pub type Result<T> = core::result::Result<T, Error>;
 
 pub type Error = Box<dyn std::error::Error>;
+
+const RENOVATE_USER: &str = "renovate[bot]";
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -17,23 +19,23 @@ async fn main() -> Result<()> {
     let personal_access_token = std::env::var("GITHUB_PERSONAL_ACCESS_TOKEN")?;
     let personal_access_token = PersonalAccessToken::new(personal_access_token);
     let config = APIConfig::with_token(personal_access_token).shared();
-    for git_repo in git_repositories.clone() {
+    for git_repo in git_repositories {
         let f = process_repo(&config, &authors, git_repo).await;
     }
     Ok(())
 }
 
-async fn process_repo(config: &SharedAPIConfig, authors: &Vec<String>, git_repo: GitRepo) -> Result<()> {
+async fn process_repo(
+    config: &SharedAPIConfig,
+    authors: &Vec<String>,
+    git_repo: GitRepo,
+) -> Result<()> {
     println!("processing repo {:?}", git_repo);
     let api: GitHubAPI = GitHubAPI::new(&config);
-
-    let pull_requests: Vec<PullRequestSimple> = api
-        .pulls
-        .list(git_repo.owner, git_repo.name)
-        .send()
-        .await?;
+    let pull_requests: Vec<PullRequestSimple> =
+        api.pulls.list(git_repo.clone().owner, git_repo.clone().name).send().await?;
     for pull_request in pull_requests {
-        if is_pr_for_me(&pull_request, &authors) {
+        if is_pr_for_me(&api, &pull_request, &authors).await? {
             println!(
                 "found pr review request : '{}' from {} by {}",
                 pull_request.title,
@@ -99,7 +101,29 @@ fn has_review_for_me(
     None
 }
 
-fn is_pr_for_me(pr: &PullRequestSimple, authors: &Vec<String>) -> bool {
+async fn is_pr_for_me(api: &GitHubAPI, pr: &PullRequestSimple, authors: &Vec<String>) -> Result<bool> {
+    let owner = pr.base.repo.owner.login.clone();
+    let repo = pr.base.repo.name.clone();
+    if (pr.user.clone().unwrap().login == RENOVATE_USER && is_none_or_empty(pr.clone().requested_reviewers)) {
+        let all_checks_good = api.checks.list_for_ref(owner.clone(), repo.clone(), pr.head.sha.clone())
+            .send().await?.check_runs.iter().all(|check|
+            check.conclusion == Some(CheckRunConclusion::Success)
+                || check.conclusion == Some(CheckRunConclusion::Skipped)
+        );
+        let no_reviews = api.pulls.list_reviews(owner.clone(), repo.clone(), pr.number).send().await?.is_empty();
+        if (all_checks_good && no_reviews) {
+            println!(
+                "renovate user without review request found: {}, pr {} on repo {} all checks done: {}",
+                pr.title, pr.number, pr.base.repo.name, all_checks_good
+            );
+            let f = api.pulls.request_reviewers(owner.clone(), repo.clone(), pr.number)
+                .body(&serde_json::json!({"reviewers": ["barakb"]}))
+                .send()
+                .await?;
+            return Ok(true);
+        }
+    }
+
     let request_me_as_reviewer = match &pr.requested_reviewers {
         Some(reviewers) => {
             reviewers.iter().any(|reviewer| reviewer.login == "barakb")
@@ -107,9 +131,16 @@ fn is_pr_for_me(pr: &PullRequestSimple, authors: &Vec<String>) -> bool {
         None => false,
     };
     let author = pr.user.clone().unwrap().login;
-    // println!("author: {}", author);
+
     let author_is_in_authors = authors.iter().any(|a| a == &author);
-    author_is_in_authors && request_me_as_reviewer
+    Ok(author_is_in_authors && request_me_as_reviewer)
+}
+
+fn is_none_or_empty<T>(reviewers: Option<Vec<T>>) -> bool {
+    match reviewers {
+        Some(reviewers) => reviewers.is_empty(),
+        None => true,
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
